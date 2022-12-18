c596fff66e989f661e83c134f394762823c88a2a
KGTH <kgthcruz94@gmail.com>
2022-12-18 17:00:37 +0000

    modificado

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User, Login, Favorite , Proveedor, Offer
+from .models import db, User, Login, Favorite, Proveedor, Offer
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
