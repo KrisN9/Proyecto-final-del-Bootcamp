1d825aa0f3f4c7bea99d44621282c4a50e73c266
KrisN9 <kristinank992@gmail.com>
2022-12-20 23:34:58 +0000

    falta posible put para favoritos

--- src/api/routes.py
+++ src/api/routes.py
@@ -17,11 +17,13 @@ def all_user():
     return jsonify(data), 200
 
 
-@api.route('/user', methods=['GET']) #se obtiene los datos de un usuario concreto
+@api.route('/user/<int: user_id>', methods=['GET']) #se obtiene los datos de un usuario concreto
 def get_user():
     user = User.query.filter_by(user_id).first()
-    data =[user.serialize() for user in users]
-    return jsonify(data), 200
+    if user: 
+        return jsonify(user.serialize()),200
+    
+    return jsonify({"Doesn´t exist"}), 400
 
 @api.route('/supplier' , methods=['GET'] )   # se obtiene todos lo proveedores
 def all_suppliers():
@@ -36,7 +38,7 @@ def get_supplier(supplier_id):
     if supplier : 
         return jsonify(supplier.serialize()),200
     
-    return jsonify({"Doesn´t exist"})
+    return jsonify({"Doesn´t exist"}), 400
 
 @api.route('/user/favorite/<int:user_id>', methods=['GET'])  #Listar todos los favoritos que pertenecen al usuario actual.
 def favorite_user(user_id):                                             #pendiente de revisar
@@ -158,3 +160,19 @@ def update_supplier(supplier_id):
             return jsonify(supplier), 200
 
         return ({"msg": 'Supplier with id {supplier_id} not found'}), 404
+
+@api.route('/offer/<int: id_offer>', methods=['PUT']) #modificar datos de oferta
+def update_offer(id_offer):
+
+    change_data = request.json
+
+    for offer in offers:
+        if offer['id'] == offer_id:
+            offer['name'] = change_data['name'],
+            offer['price'] = change_data['price']
+            offer['url'] = change_data['url']
+            offer['location'] = change_data['location']
+            return jsonify(offer), 200
+
+        return ({"msg": 'Offer with id {offer_id} not found'}), 404
+
