0f4c77d8f98df53fe325c5668be49e1c0e44c0ac
KGTH <kgthcruz94@gmail.com>
2022-12-21 12:34:29 +0000

    se actualiza

--- src/api/routes.py
+++ src/api/routes.py
@@ -9,6 +9,7 @@ from flask_jwt_extended import create_access_token
 api = Blueprint('api', __name__)
 
 
+
 @api.route('/user', methods=['GET'])  #se obtiene todo los usuarios 
 def all_user():
     users=User.query.all()
@@ -17,15 +18,7 @@ def all_user():
     return jsonify(data), 200
 
 
-@api.route('/user/<int: user_id>', methods=['GET']) #se obtiene los datos de un usuario concreto
-def get_user():
-    user = User.query.filter_by(user_id).first()
-    if user: 
-        return jsonify(user.serialize()),200
-    
-    return jsonify({"Doesn´t exist"}), 400
-
-@api.route('/supplier' , methods=['GET'] )   # se obtiene todos lo proveedores
+@api.route('/supplier' , methods=['GET'] )   # se obtiene todos los proveedores
 def all_suppliers():
     suppliers= Supplier.query.all()
     data =[supplier.serialize() for supplier in suppliers]
@@ -39,6 +32,14 @@ def get_supplier(supplier_id):
         return jsonify(supplier.serialize()),200
     
     return jsonify({"Doesn´t exist"}), 400
+
+@api.route('/user/<int:user_id>', methods=['GET']) # se obtiene usuario por id 
+def get_supplier(user_id):
+    users= User.query.filter_by(id=user_id).first()
+    if users : 
+        return jsonify(users.serialize()),200
+    
+    return jsonify({"Doesn´t exist"})
 
 @api.route('/user/favorite/<int:user_id>', methods=['GET'])  #Listar todos los favoritos que pertenecen al usuario actual.
 def favorite_user(user_id):                                             #pendiente de revisar
@@ -55,10 +56,10 @@ def offer_supplier(supplier_id):                                                
 @api.route('/offer', methods=['GET'])  #obtiene todas las ofertas  # revisar
 def all_offer():
     offers= Offer.query.all()
-    data =[offer.serialize() for Offer in offers]
+    data =[offer.serialize() for offer in offers]
     return jsonify(data), 200
 
-@api.route('/favorite', methods=['GET'])  #obtiene todas los favoritos 
+@api.route('/favorite', methods=['GET'])  #obtiene todos los favoritos 
 def all_favorite():
     favorites= Favorite.query.all()
     data =[favorite.serialize() for favorite in favorites]
@@ -75,7 +76,7 @@ def delete_user(user_id):
     except:
         return jsonify({"message": "Error"}), 400
     
-    return jsonify({"message": "User Deleted."})
+    return jsonify({"message": "User Deleted."}),200
 
 @api.route('/delete_supplier/<int:supplier_id>', methods=['DELETE'])  #emilinar proveedor por id 
 def delete_supplier(supplier_id):
@@ -210,3 +211,4 @@ def update_favorite(favorite_id):
             return jsonify(favorite), 200
 
         return jsonify({"msg": "Favorite with id {favorite_id} is not found"}), 404
+
