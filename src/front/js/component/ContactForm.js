8b3fa391471af6328e6cdf3f9fe2f376dbf6f44a
KrisN9 <kristinank992@gmail.com>
2022-12-22 23:07:12 +0000

    creado formulario de contacto, correcciones pendientes

--- src/front/js/component/ContactForm.js
+++ src/front/js/component/ContactForm.js
@@ -2,15 +2,18 @@ import React from "react";
 
 const ContactForm = () => {
   return (
-    <div className="container">
+    <>
+    <h1>Contacta con nosotros</h1> {/* falta corregir estilo de la letra*/}
+	<h3>Rellena el formulario y contactaremos contigo</h3> {/* falta corregir estilo de la letra*/}
+    <div className="container bg-warning px-3 py-3">
       <div className="form-floating mb-3">
         <input
           type="name"
           className="form-control"
           id="floatingName"
-          placeholder="Name and Last Name"
+          placeholder="Nombre y Apellidos"
         />
-        <label for="floatingName">Name and Last Name*</label>
+        <label for="floatingName">Nombre y Apellidos*</label>
       </div>
       <div className="form-floating mb-3">
         <input
@@ -19,29 +22,39 @@ const ContactForm = () => {
           id="floatingInput"
           placeholder="name@example.com"
         />
-        <label for="floatingInput">Email address*</label>
+        <label for="floatingInput">Dirección de correo electrónico*</label>
       </div>
       <div className="form-floating mb-3">
         <input
           type="phone-number"
           className="form-control"
           id="floatingNumber"
-          placeholder="Phone Number"
+          placeholder="Número de teléfono"
         />
-        <label for="floatingNumber">Phone Number</label>
+        <label for="floatingNumber">Número de teléfono</label>
       </div>
-      <div className="form-floating mb-3">
-        <label for="exampleFormControlMessage" class="form-label">
-          Message*
+      <div className="form-floating mb-3">       {/* corregir el campo de mensaje */}
+        <label for="exampleFormControlMessage" className="form-label">
+          Mensaje*
         </label>
         <textarea
           class="form-control"
           id="exampleFormControlMessage"
-          placeholder="Message"
+          placeholder="Mensaje"
           rows="3"
         ></textarea>
       </div>
+      <div className="form-check mb-3">
+        <input className="form-check-input" type="checkbox" value="" id="defaultCheck1" />
+        <label className="form-check-label" for="defaultCheck1">
+            He leído y consiento al contenido de la Información Legal y de Protección de Datos.
+        </label>
+        </div>
+        <div className="col-12 mb-3">
+            <button type="submit" className="btn btn-danger">Enviar</button>
+        </div>
     </div>
+    </>
   );
 };
 
