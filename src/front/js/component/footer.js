140e3be65aebe77357d49de8ec2e3a0577e70897
KGTH <kgthcruz94@gmail.com>
2022-12-22 17:53:31 +0000

    me modifica footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -3,27 +3,42 @@ import React, { Component } from "react";
 export const Footer = () => (
 
 
-	<footer className="footer mt-auto py-3 text-center bg-warning text-dark">
-		<div className="row">
-			<div className="col-12 col-md-3">Colaboradores	</div>
-
-			
-			
-			<div className="col-12 col-md-3 ">Descarga nuestras Apps
-		<div className=" "> 
-		<img src="https://media.news9live.com/h-upload/2021/10/25/43386-google-play-store.jpg" width="40" height="40"/>
-		<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Apple_Store_logo.svg/2048px-Apple_Store_logo.svg.png" width="40" height="40"/>
-		</div>
-		
+	<footer className="footer mt-auto py-3 text-center text-dark ">
+		<div className="row ">
+			<div className="col-12 col-md-3 ">Colaboradores
+			<div className="mt-3"> 
+			<img src="https://pbs.twimg.com/profile_images/1531998332347990017/HGRMiQ___400x400.jpg" width="40" height="40" class="rounded-circle me-2"/>
+			<img src="https://upload.wikimedia.org/wikipedia/commons/c/c0/The_Good_Burger_%28TGB%29.jpg" width="40" height="40" class="rounded-circle me-2"/>
+			<img src="https://www.telepizza.es/Content/Themes/TP/Images/meta-image.jpg" width="40" height="40" class="rounded-circle me-2"/>
+			<img src="https://noticiaslogisticaytransporte.com/wp-content/uploads/2022/12/oficinas-glovo-barcelona.png" width="40" height="40" class="rounded-circle me-2"/>
+			<img src="https://assets.stickpng.com/images/5f4921b668ecc70004ae7047.png" width="40" height="40" class="rounded-circle me-2" />
+			<img src="https://play-lh.googleusercontent.com/7hm-g4MVs5eJOOYv1mECDpPU70mnZ17ixVWNeftIwAk6T_EEM3czjHecd4NO-QwFaGWp" width="40" height="40" class="rounded-circle me-2"/>
+			</div>
+			<div className="mt-2"> 
+			<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Burger_King_logo_%281999%29.svg/2024px-Burger_King_logo_%281999%29.svg.png" width="40" height="40" class="rounded-circle me-2"/>
+			<img src="https://i.pinimg.com/originals/ee/fd/cf/eefdcf8f23c277bfac155152c6ab3a20.jpg" width="40" height="40" class="rounded-circle me-2"/>
+			<img src="https://assets.stickpng.com/images/5842eff2a6515b1e0ad75b0e.png" width="40" height="40" class="rounded-circle me-2"/>
+			<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/McDonald%27s_logo.svg/2560px-McDonald%27s_logo.svg.png" width="40" height="40" class="rounded-circle me-2"/>
+			<img src="https://www.elcorteingles.es/centroscomerciales/uploads/service/logo/936/malvon%20logo.jpg" width="40" height="40"  class="rounded-circle me-2"/>
+			<img src="https://img2.freepng.es/20180527/cjp/kisspng-papa-john-s-pizza-take-out-restaurant-finger-post-5b0ac8dbe21378.690054431527433435926.jpg" width="40" height="40" class="rounded-circle me-2"/>
+			</div>
+			</div>
+			<div className="col-12 col-md-3  ">Descarga nuestras Apps
+			<div className="mt-3"> 
+			<img src="https://es.logodownload.org/wp-content/uploads/2019/06/disponible-en-google-play-badge-1.png" width="100" height="40" class=" me-2"/>
+			<img src="https://vivecudillero.com/wp-content/uploads/2019/11/appstore.png" width="100" height="40" class="me-2"/>
+			</div>
 			</div>		
-			<div className="col-12 col-md-3">Comunidad
-			<div>
-			<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/2048px-Facebook_f_logo_%282019%29.svg.png" width="40" height="40"/>
-		    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/2048px-Instagram_logo_2016.svg.png" width="40" height="40"/>
-			<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Twitter-logo.svg/2491px-Twitter-logo.svg.png" width="40" height="40"/>
+			<div className="col-12 col-md-3 ">Comunidad
+			<div className=" mt-3">
+			<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/2048px-Facebook_f_logo_%282019%29.svg.png" width="40" height="40" class="rounded-circle me-2"/>
+		    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/2048px-Instagram_logo_2016.svg.png" width="40" height="40" class="rounded-circle me-2"/>
+			<img src="https://www.pngkey.com/png/full/2-27646_twitter-logo-png-transparent-background-logo-twitter-png.png" width="40" height="40" class="rounded-circle me-2"/>
 			</div>
 			</div>
-			<div className="col-12 col-md-3">Tienes alguna duda? Contactanos</div>
+			<div className="col-12 col-md-3 ">Tienes alguna duda? 
+			<a href="#" class="text-decoration-none"> Contactanos.</a>
+			</div>
 		</div>
 
 	</footer>
