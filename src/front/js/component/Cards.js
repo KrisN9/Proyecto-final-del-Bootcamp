72de452877c3a01e50a068058c9bb83cc8d122a5
KGTH <kgthcruz94@gmail.com>
2023-01-02 16:24:45 +0000

    se crea cards/revisar

---
+++ src/front/js/component/Cards.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Cards =()=>{
+
+    return (
+
+         /* pendiente de revisar si van las cards */ 
+         
+        <div className="card mb-3">
+        <div className="row g-0">
+            <div className="col-md-3">
+            <img src="https://www.dominospizza.es/images/2x1alabama_delivery_RESP.jpg" className="img-fluid rounded-start" alt="..."/>
+            </div>
+            <div className="col-md-7">
+            <div className="card-body text-center">
+                <h5 className="card-title fs-3 fw-bolder">Dominos Pizza</h5>
+                <p className="card-text"> 2x1 En todas las masas y tamaños</p>
+            </div>
+            </div>
+            <div className="col-md-2 mt-5">
+                <button type="button" class="btn btn-danger float-end me-3">
+                <i class="fas fa-heart"></i>
+                </button>
+                <button type="button" class="btn btn-warning float-end me-2">Pedir!</button>
+                </div>
+        </div>
+        </div>
+     
+
+
+    )
+
+}
+
+
+
+
+export default Cards; 
