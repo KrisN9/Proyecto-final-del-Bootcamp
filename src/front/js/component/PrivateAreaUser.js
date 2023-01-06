688f3f5a449c54c6585204720540493fa90d5d2b
KrisN9 <kristinank992@gmail.com>
2023-01-06 19:47:27 +0000

    probando pautas para crear area privada

--- src/front/js/component/PrivateAreaUser.js
+++ src/front/js/component/PrivateAreaUser.js
@@ -1,17 +1,12 @@
 import React from "react";
 
 const PrivateAreaUser=()=>{
-const isLogged = false;
 return (
 
 
     <div className="container">
 
         area privada de usuarios
-        {
-            isLogged ? <Link to='/logout'>Cerrar sesión</Link>
-            : <Link to='/inicio-sesion'>Iniciar sesión</Link>
-        }
     </div>
 )
    
