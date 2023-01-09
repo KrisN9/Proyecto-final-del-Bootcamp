c4f716bf09d46cdb383eb2dd53b315f74f7dc412
KrisN9 <kristinank992@gmail.com>
2023-01-09 21:33:58 +0000

    modificacion en models

--- src/api/models.py
+++ src/api/models.py
@@ -79,14 +79,14 @@ class Supplier(db.Model):
             "id": self.id,
             "name": self.name,
             "last name": self.last_name,
-            
+            "company name": self.company_name,
         }
 
 
 class Offer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     id_supplier =db.Column(db.Integer, db.ForeignKey('supplier.id'),nullable=False)
-    login= db.relationship('Supplier', backref='offer', lazy=True) 
+    login= db.relationship('Supplier', backref='supplier', lazy=True) 
     name=db.Column(db.String(80), unique=False, nullable=False)
     price= db.Column(db.Integer, nullable=False )
     url= db.Column(db.String(200), nullable=False)
