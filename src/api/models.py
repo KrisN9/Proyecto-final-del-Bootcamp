b72d9cdca03955b439c4125f7615a39eeab0ecc6
KGTH <kgthcruz94@gmail.com>
2023-01-10 17:50:36 +0000

    pequeña modificacion de models

--- src/api/models.py
+++ src/api/models.py
@@ -6,7 +6,7 @@ class Login(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    type_user= db.Column(db.Integer)
+    type_user= db.Column(db.Integer) # pendiente de revisar 
 
     def __repr__(self):
         return f'<Login {self.email}>'
@@ -28,7 +28,7 @@ class User(db.Model):
     city=db.Column(db.String(80), unique=False, nullable=False)
     telephone_number=db.Column(db.String(80), nullable=False) 
     id_login= db.Column(db.Integer, db.ForeignKey('login.id'), nullable=False)
-    login= db.relationship('Login', backref='user', lazy=True) 
+    login= db.relationship('Login', backref='login', lazy=True) 
     
     
     def __repr__(self):
@@ -37,7 +37,7 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "email": self.email, # revisar
 
         }
 
@@ -45,9 +45,9 @@ class User(db.Model):
 class Favorite(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     id_user = db.Column(db.Integer, db.ForeignKey('user.id'),nullable=False)
-    login_user= db.relationship('Login', backref='user', lazy=True) 
+    user= db.relationship('User', backref='user', lazy=True) 
     id_offer =db.Column(db.Integer, db.ForeignKey('offer.id'),nullable=False)
-    login_offer= db.relationship('Offer', backref='offer', lazy=True)
+    offer= db.relationship('Offer', backref='offer', lazy=True)
 
     def __repr__(self):
         return f'<Favorite {self.email}>'
@@ -79,14 +79,14 @@ class Supplier(db.Model):
             "id": self.id,
             "name": self.name,
             "last name": self.last_name,
-            
+            "company name": self.company_name,
         }
 
 
 class Offer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     id_supplier =db.Column(db.Integer, db.ForeignKey('supplier.id'),nullable=False)
-    login= db.relationship('Supplier', backref='offer', lazy=True) 
+    login= db.relationship('Supplier', backref='supplier', lazy=True) 
     name=db.Column(db.String(80), unique=False, nullable=False)
     price= db.Column(db.Integer, nullable=False )
     url= db.Column(db.String(200), nullable=False)
