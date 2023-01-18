aa93ca69e586b73cd7719a609e4518801ac70804
KrisN9 <kristinank992@gmail.com>
2023-01-18 23:45:22 +0000

    zoom del mapa arreglado

--- src/front/js/component/MyMapComponent.js
+++ src/front/js/component/MyMapComponent.js
@@ -21,7 +21,7 @@ function MyMapComponent() {
 
   const onLoad = React.useCallback(function callback(map) {
     const bounds = new window.google.maps.LatLngBounds(center);
-    map.fitBounds(bounds);
+    /* map.fitBounds(bounds); */
 
     setMap(map)
   }, [])
@@ -34,7 +34,7 @@ function MyMapComponent() {
       <GoogleMap
         mapContainerStyle={containerStyle}
         center={center}
-        zoom={10}
+        zoom={15}
         onLoad={onLoad}
         onUnmount={onUnmount}
       >
