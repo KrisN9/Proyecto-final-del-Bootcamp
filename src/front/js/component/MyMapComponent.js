2d4eb57c165d7f76800f57ae1482dfe6d14f838d
KrisN9 <kristinank992@gmail.com>
2023-01-18 23:36:02 +0000

    mapa mejorado, falta arreglar el zoom

--- src/front/js/component/MyMapComponent.js
+++ src/front/js/component/MyMapComponent.js
@@ -1,59 +1,47 @@
 import React from "react";
-import { compose, withProps } from "recompose";
-import {
-  withScriptjs,
-  withGoogleMap,
-  GoogleMap,
-  Marker,
-} from "react-google-maps";
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 
-const MyMapComponent = compose(
-  withProps({
-    googleMapURL:
-      "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
-    loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `300px` }} />,
-    mapElement: <div style={{ height: `142%` }} />,
-  }),
-  withScriptjs,
-  withGoogleMap
-)((props) => (
-  <GoogleMap defaultZoom={14} defaultCenter={{ lat: 40.417213416794496, lng: -3.7036235136715936 }}>
-    {props.isMarkerShown && (
-      <Marker position={{ lat: 40.417213416794496, lng: -3.7036235136715936 }} onClick={props.onMarkerClick}/>
-    )}
-  </GoogleMap>
-));
+const containerStyle = {
+  width: '100%',
+  height: '425px'
+};
 
-class MyFancyComponent extends React.PureComponent {
-  state = {
-    isMarkerShown: false,
-  }
+const center = {
+  lat: 40.41397175226467,
+  lng: -3.70315046264256
+};
 
-  componentDidMount() {
-    this.delayedShowMarker()
-  }
+function MyMapComponent() {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: "NEXT_PUBLIC_GOOGLE_MAPS_API_KEY"
+  })
 
-  delayedShowMarker = () => {
-    setTimeout(() => {
-      this.setState({ isMarkerShown: true })
-    }, 3000)
-  }
+  const [map, setMap] = React.useState(null)
 
-  handleMarkerClick = () => {
-    this.setState({ isMarkerShown: false })
-    this.delayedShowMarker()
-  }
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
 
+    setMap(map)
+  }, [])
 
-  render() {
-    return (
-      <MyMapComponent
-        isMarkerShown={this.state.isMarkerShown}
-        onMarkerClick={this.handleMarkerClick}
-      />
-    )
-  }
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+  return isLoaded ? (
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+      >
+        { /* Child components, such as markers, info windows, etc. */ }
+        <></>
+      </GoogleMap>
+  ) : <></>
 }
 
 export default MyMapComponent;
