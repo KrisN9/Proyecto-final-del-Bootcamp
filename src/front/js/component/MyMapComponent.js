90f7a49c39191b209a53d0e7be605c15dc0c539a
KrisN9 <kristinank992@gmail.com>
2023-01-20 18:08:18 +0000

    nueva funcion en mapa, faltan detalles para visualizar input

--- src/front/js/component/MyMapComponent.js
+++ src/front/js/component/MyMapComponent.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
-/* import usePlacesAutocomplete, {getGeocode, getLatLng,} from "use-places-autocomplete"; */
-/* import { Combobox, ComboboxInput, ComboboxPopover, ComboboxList, ComboboxOption } from "@reach/combobox"; */
+import usePlacesAutocomplete, {getGeocode, getLatLng,} from "use-places-autocomplete";
+import { Combobox, ComboboxInput, ComboboxPopover, ComboboxList, ComboboxOption } from "@reach/combobox";
 
 const containerStyle = {
   width: '100%',
@@ -45,5 +45,46 @@ function MyMapComponent() {
       </GoogleMap>
   ) : <></>
 }
+
+function Search() {
+  const {
+    ready,
+    value,
+    suggestions: { status, data },
+    setValue,
+    clearSuggestions,
+  } = usePlacesAutocomplete({
+    requestOptions: {
+      location: { lat: () => 40.41397175226467, lng: () => -3.70315046264256 },
+      radius: 200 * 1000,
+    },
+  });
+
+  return (
+    <div className="search">
+      <Combobox
+        onSelect={(address) => {
+          console.log(address);
+        }}
+      >
+        <ComboboxInput
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+          disabled={!ready}
+          placeholder="Tu búsqueda aquí..."
+        />
+        <ComboboxPopover>
+          {status === "OK" &&
+            data.map(({ id, description }) => (
+              <ComboboxOption key={id} value={description} />
+            ))}
+        </ComboboxPopover>
+      </Combobox>
+    </div>
+  );
+}
+
 
 export default MyMapComponent;
