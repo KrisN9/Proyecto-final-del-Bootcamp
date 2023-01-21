4c6c78b90a5cdb74ce49141c63e560ab3fce9959
KrisN9 <kristinank992@gmail.com>
2023-01-21 22:01:04 +0000

    creando mapa para testear via mapbox

--- src/front/js/component/TestMap.js
+++ src/front/js/component/TestMap.js
@@ -1,113 +1,35 @@
-import React, { useCallback, useState } from "react";
-import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
-import usePlacesAutocomplete from "use-places-autocomplete";
-import {
-  Combobox,
-  ComboboxInput,
-  ComboboxOption,
-  ComboboxPopover,
-} from "@reach/combobox";
+import React, { Component } from "react";
+import reactDom from "react-dom";
+import mapboxgl from "mapbox-gl";
 
-const libraries = ["places"];
-const mapContainerStyle = {
-  width: "100%",
-  height: "465px",
-};
+class TestMap extends React.Component{
 
-const center = {
-  lat: 40.41397175226467,
-  lng: -3.70315046264256,
-};
+  constructor(props){
+    super(props);
+    this.state = {
+      lng: -3.70315046264256,
+      lat: 40.41397175226467,
+      zoom: 14
+    }
+  }
 
-const options = {
-  zoomControl: true,
-};
+  componentDidMount(){
+    const map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [this.state.lng, this.state.lat],
+      zoom: this.state.zoom
+    })
+  }
 
-export default function TestMap() {
-  const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
-    libraries,
-  });
+  render(){
+    return (
+      <div>
+        <div ref={el => this.mapContainer = el} style={{width:'100%', height:'465px'}} />
+      </div>
+    )
+  }
 
-  const [markers, setMarkers] = useState([]);
-  const [selected, setSelected] = useState(null);
-
-  const onMapClick = useCallback((event) => {
-    setMarkers((current) => [
-      ...current,
-      {
-        lat: event.latLng.lat(),
-        lng: event.latLng.lng(),
-        time: new Date(),
-      },
-    ]);
-  }, []);
-
-  const mapRef = useRef();
-  const onMapLoad = useCallback((map) => {
-    mapRef.current = map;
-  }, []);
-
-  if (loadError) return "Error loading maps";
-  if (!isLoaded) return "Loading Maps";
-
-  return (
-    <div>
-      <Search />
-
-      <GoogleMap
-        mapContainerStyle={mapContainerStyle}
-        zoom={15}
-        center={center}
-        options={options}
-        onClick={onMapClick}
-        onLoad={onMapLoad}
-      >
-        {/* {markers.map(marker => <Marker key={marker.time.toISOstring()} position={{lat: marker.lat, lng: marker.lng}} />)}
-        onClick={() => {
-          setSelected(marker);
-        }} */}
-      </GoogleMap>
-    </div>
-  );
 }
 
-function Search() {
-  const {
-    ready,
-    value,
-    suggestions: { status, data },
-    setValue,
-    clearSuggestions,
-  } = usePlacesAutocomplete({
-    requestOptions: {
-      location: { lat: () => 40.41397175226467, lng: () => -3.70315046264256 },
-      radius: 200 * 1000,
-    },
-  });
-
-  return (
-    <div className="search">
-      <Combobox
-        onSelect={(address) => {
-          console.log(address);
-        }}
-      >
-        <ComboboxInput
-          value={value}
-          onChange={(e) => {
-            setValue(e.target.value);
-          }}
-          disabled={!ready}
-          placeholder="Tu búsqueda aquí..."
-        />
-        <ComboboxPopover>
-          {status === "OK" &&
-            data.map(({ id, description }) => (
-              <ComboboxOption key={id} value={description} />
-            ))}
-        </ComboboxPopover>
-      </Combobox>
-    </div>
-  );
-}
+export default TestMap;
