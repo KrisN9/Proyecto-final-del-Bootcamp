caf0c38512678e6a6c7a3a35e6b380b7aa51e56e
KrisN9 <kristinank992@gmail.com>
2023-01-22 19:49:41 +0000

    añadido control de zoom y geolocalizacion, faltan iconos y crear marcador

--- src/front/js/component/TestMap.js
+++ src/front/js/component/TestMap.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import reactDom from "react-dom";
 import mapboxgl from "mapbox-gl";
 import Autofill from "./Autofill";
-import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
+/* import cities from './data/cities'; */
+
 
 mapboxgl.accessToken='pk.eyJ1Ijoia3Jpc245IiwiYSI6ImNsZDV4Y2x0ZTByOHIzb2tianpoZ2xmeWgifQ.M8N3QZtBSFlC_MPoI-PVTQ';
 
@@ -25,21 +26,12 @@ class TestMap extends React.Component{
       zoom: this.state.zoom
     })
 
-    const marker = new mapboxgl.Marker() // Initialize a new marker
-    .setLngLat([80.632416, 7.29163]) // Marker [lng, lat] coordinates
-    .addTo(map); // Add the marker to the map
-  
-    const geocoder = new MapboxGeocoder({ // Initialize the geocoder
-      accessToken: mapboxgl.accessToken, // Set the access token
-      mapboxgl: mapboxgl, // Set the mapbox-gl instance
-      marker: false, // Do not use the default marker style
-      placeholder: 'Search for places in Sri Lanka', // Placeholder text for the search bar
-      bbox: [79.0000, 5.0000, 82.0000, 10.0000], // Sri lanka 
-      proximity: {
-        longitude: -122.25948,
-        latitude: 37.87221
-      } 
-  });
+    map.addControl(new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken
+  }));
+
+    map.addControl(new mapboxgl.FullscreenControl());
+    map.addControl(new mapboxgl.NavigationControl());
 
   }
 
@@ -47,7 +39,7 @@ class TestMap extends React.Component{
     return (
       <>
       <div>
-        <Autofill />
+        {/* <Autofill /> */}
       </div>
       <div>
         <div ref={el => this.mapContainer = el} style={{width:'100%', height:'465px'}} />
