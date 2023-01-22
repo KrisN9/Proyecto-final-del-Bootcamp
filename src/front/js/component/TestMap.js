c766a15f685777422865840a375162c4fb6c98b4
KrisN9 <kristinank992@gmail.com>
2023-01-22 18:48:45 +0000

    añadiendo funciones para marcador y geolocalización

--- src/front/js/component/TestMap.js
+++ src/front/js/component/TestMap.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import reactDom from "react-dom";
 import mapboxgl from "mapbox-gl";
 import Autofill from "./Autofill";
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
 
 mapboxgl.accessToken='pk.eyJ1Ijoia3Jpc245IiwiYSI6ImNsZDV4Y2x0ZTByOHIzb2tianpoZ2xmeWgifQ.M8N3QZtBSFlC_MPoI-PVTQ';
 
@@ -24,13 +25,21 @@ class TestMap extends React.Component{
       zoom: this.state.zoom
     })
 
-/*  const marker = new mapboxgl.Marker({
-    draggable: true
-    })
-    .setLngLat([0, 0])
-    .addTo(map);
-
-		}) */
+    const marker = new mapboxgl.Marker() // Initialize a new marker
+    .setLngLat([80.632416, 7.29163]) // Marker [lng, lat] coordinates
+    .addTo(map); // Add the marker to the map
+  
+    const geocoder = new MapboxGeocoder({ // Initialize the geocoder
+      accessToken: mapboxgl.accessToken, // Set the access token
+      mapboxgl: mapboxgl, // Set the mapbox-gl instance
+      marker: false, // Do not use the default marker style
+      placeholder: 'Search for places in Sri Lanka', // Placeholder text for the search bar
+      bbox: [79.0000, 5.0000, 82.0000, 10.0000], // Sri lanka 
+      proximity: {
+        longitude: -122.25948,
+        latitude: 37.87221
+      } 
+  });
 
   }
 
