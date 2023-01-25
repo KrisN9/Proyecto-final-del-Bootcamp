88af993cb89cf940de3d918aeacb2d5ad773c23f
KrisN9 <kristinank992@gmail.com>
2023-01-25 13:38:32 +0000

    modificaciones en modal

--- src/front/js/component/PrivateAreaSupplier.js
+++ src/front/js/component/PrivateAreaSupplier.js
@@ -5,6 +5,7 @@ import { useContext } from "react";
 import OfferCards from "./OfferCards";
 import Supplier from "./Supplier";
 import { useNavigate } from "react-router-dom";
+import SupplierEdit from "./componentes de prueba/SupplierEdit";
 
 
 const PrivateAreaSupplier=()=>{
@@ -45,14 +46,14 @@ return (
   {/* pendiente saber donde movemos el botón del modal */}
 
   <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div className="modal-dialog">
+    <div className="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable">
       <div className="modal-content">
         <div className="modal-header">
           <h5 className="modal-title" id="exampleModalLabel">Modificar datos de cuenta</h5>
           <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div className="modal-body">
-          ...
+          <SupplierEdit />
         </div>
         <div className="modal-footer">
           <button type="button" className="btn btn-outline-secondary" data-bs-dismiss="modal">Cancelar</button>
--- src/front/js/component/componentes de prueba/SupplierEdit.js
+++ src/front/js/component/componentes de prueba/SupplierEdit.js
@@ -2,21 +2,25 @@ import React ,{ useState, useEffect, useContext }from "react";
 
 
 const SupplierEdit = () => {
-  const {store, actions} = useContext(Context);
+  /* const {store, actions} = useContext(Context); */
   const [currentSupplier, setCurrentSupplier] = useState({});
   const [oldSupplier, setOldSupplier] = useState({});
 
   return (
     <div className="container">
-      <input type="supplier_name" onChange={(e) => {
-        setCurrentSupplier({...supplier, name: e.target.value});
-      }}
-      name="floating_username"
-      defaultValue={oldSupplier.name}
-      required
-      />
-      <label htmlFor="supplier_name">Nombre:</label>
-
+      <div>
+        <label htmlFor="supplier_name">Nombre: </label>
+        <input type="supplier_name" onChange={(e) => {
+          setCurrentSupplier({...supplier, name: e.target.value});
+        }}
+        name="floating_username"
+        defaultValue={oldSupplier.name}
+        required
+        />
+      </div>
+        <br></br>
+      <div>
+      <label htmlFor="supplier_company_name">Empresa: </label>
       <input type="supplier_company_name" onChange={(e) => {
         setCurrentSupplier({...supplier, company_name: e.target.value});
       }}
@@ -24,8 +28,10 @@ const SupplierEdit = () => {
       defaultValue={oldSupplier.company_name}
       required
       />
-      <label htmlFor="supplier_company_name">Empresa:</label>
-
+      </div>
+        <br></br>
+      <div>
+      <label htmlFor="supplier_email">Correo electrónico: </label>
       <input type="supplier_email" onChange={(e) => {
         setCurrentSupplier({...supplier, email: e.target.value});
       }}
@@ -33,8 +39,10 @@ const SupplierEdit = () => {
       defaultValue={oldSupplier.email}
       required
       />
-      <label htmlFor="supplier_email">Correo electrónico:</label>
-
+      </div>
+        <br></br>
+      <div>
+      <label htmlFor="supplier_company_number">Cif de empresa: </label>
       <input type="supplier_company_number" onChange={(e) => {
         setCurrentSupplier({...supplier, company_cif: e.target.value});
       }}
@@ -42,7 +50,8 @@ const SupplierEdit = () => {
       defaultValue={oldSupplier.company_cif}
       required
       />
-      <label htmlFor="supplier_company_number">Cif de empresa:</label>
+        <br></br>
+      </div>
     </div>
   )
 
