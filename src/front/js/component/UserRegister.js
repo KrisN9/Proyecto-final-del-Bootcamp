b9e2f9d85a7b630b978a85296577c34c482a7907
KGTH <kgthcruz94@gmail.com>
2023-01-29 12:01:32 +0000

    pendiente probar modal

--- src/front/js/component/UserRegister.js
+++ src/front/js/component/UserRegister.js
@@ -196,10 +196,31 @@ const UserRegister = () => {
             </label>
           </div>
           <div className="col-12 mb-3">
-            <button type="submit" className="btn btn-danger">
+            <button type="submit" className="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
               Enviar
             </button>
           </div>
+
+
+          {/* pendiente probar modal
+
+          <div class="modal" tabindex="-1">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <p>Modal body text goes here.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div> */}
         </div>
       </form>
     </>
