e2c3ff384d276a861fd10bff8edb17bdc781b612
KrisN9 <kristinank992@gmail.com>
2023-01-30 13:21:17 +0000

    modificaciones en sidebar y marcador, a revisar cuando backend funcione

--- src/front/js/component/componentes de prueba/Sidebar.js
+++ src/front/js/component/componentes de prueba/Sidebar.js
@@ -1,21 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const Sidebar = () => {
+
+	const [city, setCity] = useState([]);
 
 	useEffect(() => {
 		fetch(process.env.BACKEND_URL + "/api/city")
 		.then((response) => response.json())
 		.then((response) => {
 			console.log(response);
-		})
-	})
+			setCity(response);
+		});
+	}, []);
 
     return (
-        <div class='sidebar'>
-		<div class='heading'>
-			<h3>Ofertas en:</h3>
+        <div className='sidebar'>
+		<div className='heading'>
+			<h3>Ofertas en:	</h3>
 		</div>
-		<div id='listings' class='listings'></div>
+			{city.map((value, index) => {
+				return (<div id='listings' className='listings' key={index} value={value.id}>{value.name}</div>);
+			})}
 		</div>
     )
 }
