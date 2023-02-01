27b2d9adc1ff20bce045974acef0e8b03239d04c
KGTH <kgthcruz94@gmail.com>
2023-02-01 16:07:48 +0000

    se encuentra error y se modifica

--- src/front/js/component/Supplier.js
+++ src/front/js/component/Supplier.js
@@ -52,7 +52,7 @@ const Supplier = () => {
         <div className="modal-dialog modal-dialog-scrollable">
           <div className="modal-content">
             <div className="modal-header">
-              <h1 className="modal-title fs-5" id="staticBackdropLabel">Editar tus datos</h1>
+              <h1 className="modal-title fs-5" id="staticBackdropLabel">Edita tus datos</h1>
               <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div className="modal-body">
