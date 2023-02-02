2efab8418ea8cb0cf1085a4305df7195c024663a
KrisN9 <kristinank992@gmail.com>
2023-02-02 20:31:50 +0000

    devueltas cards al estado inicial, boton favoritos funciona, de momento para redirigir

--- src/front/js/component/Cards.js
+++ src/front/js/component/Cards.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 
 const Cards = () => {
-  /* const [favorites, setFavorites] = useState([]) */
+  const [favorites, setFavorites] = useState([])
   
-/*   useEffect(() => {
+  useEffect(() => {
     const optionFavorite = {
       headers: {
         "Content-Type": "application/json",
@@ -36,32 +36,33 @@ const Cards = () => {
     }).then((response)=>{
       console.log(response)
     })
-  }  */
+  } 
 
 
   return (
    
     <div className="container col-12 col-md-6 mt-4"> 
-    <div className="mt-3"><p className="text-center fst-italic fs-3 text-danger"> Tienes {/* {favorites.length} */} favoritos.</p>
+    <div className="mt-3"><p className="text-center fst-italic fs-3 text-danger"> Tienes {favorites.length} favoritos.</p>
  
-        <div className="card mb-3" /* key={index} */>
+        { favorites.map ((favorite, index)=>{
+           return <div className="card mb-3" key={index}>
                   <div className="row g-0">
                 <div className="col-md-3">
                   <img
-                    src="..."
+                    src={favorite.url_image}
                     className="img-fluid rounded-start"
                     alt="..."
                   />
                 </div>
                 <div className="col-md-7">
                   <div className="card-body text-center">
-                    <h5 className="card-title fs-3 fw-bolder">...</h5>
-                    <p className="card-text">...</p>
+                    <h5 className="card-title fs-3 fw-bolder">{favorite.company_name}</h5>
+                    <p className="card-text"> {favorite.title}</p>
                   </div>
                 </div>
                 <div className="col-md-2 mt-2">
-                <p>Precio: €</p>
-                  <a href="..." target="_blanck" type="button" className="btn btn-warning float-end me-2 " >
+                <p>Precio:{favorite.price}€</p>
+                  <a href={favorite.url} target="_blanck" type="button" className="btn btn-warning float-end me-2 " >
                     Pedir!
                   </a>
                   <button type="button"  className="btn btn-outline-secondary mt-2 mb-2" onClick={()=>removeFavorite(favorite.id)}>
@@ -71,6 +72,9 @@ const Cards = () => {
                 </div>
               </div>
             </div>
+
+
+        })}
      </div>
     </div>
   );
