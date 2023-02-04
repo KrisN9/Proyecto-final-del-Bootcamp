59a6ba653753dfb8d34342403ce14f712cac3ec7
KGTH <kgthcruz94@gmail.com>
2023-02-04 18:38:25 +0000

    navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -14,40 +14,41 @@ export const Navbar = () => {
           }
   
 
-	return (
+		  return (
 					
-		<nav className="navbar navbar-expand-lg navbar-dark text-dark">
-			  <div className="container ">
-
-   				<Link to="/">
-					<span className="navbar-brand mb-0 h1">
-					<img src="https://i.postimg.cc/C12Xn8J5/Promo-Food.png" alt="Logo" width="70" height="70" className="d-inline-block align-text-top rounded-circle"/>
-					</span>
-				</Link>
-				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-				<span className="navbar-toggler-icon"></span>  
-				</button>
-				<div className="collapse navbar-collapse gap-2 justify-content-md-end" id="navbarSupportedContent"> 
-		        
-				{localStorage.getItem('token') ? <> 
-				<Link  to={ localStorage.getItem("type")== "user" ?"/area-privada-usuario" : "/area-privada-proveedor"}> 
-				   
-				</Link>
-				  <Link className="btn btn-outline-success rounded-pill" to="/area-privada-usuario">Área privada</Link>   {/* Terminar de implantar para user/supplier, y que solo aparezca en la Home */}
-				  <Link className="btn btn-outline-success rounded-pill" to="/area-de-favoritos">Mis favoritos</Link>
-				  <Link className="btn btn-outline-success rounded-pill" to="/" onClick={SessionOut}>Cerrar sesión</Link>
-				</> 
-				 :<>
-				<Link className="btn btn-outline-success rounded-pill " to="/registro"> 
-				   Registrarse
-				</Link>
-				   <Link className="btn btn-outline-success rounded-pill " to="/inicio-sesion">
-				   Iniciar sesión
-				   </Link> </>
-     				 }
-			      </div>
-				</div>
-				</nav>
-						
-	);
-};
+			<nav className="navbar navbar-expand-lg navbar-dark text-dark">
+				  <div className="container ">
+	
+					   <Link to="/">
+						<span className="navbar-brand mb-0 h1">
+						<img src="https://i.postimg.cc/KYLQqTks/Promo-Food-1.png" alt="Logo" width="70" height="70" className="d-inline-block align-text-top rounded-circle"/>
+						</span>
+					</Link>
+					<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>  
+					</button>
+					<div className="collapse navbar-collapse gap-2 justify-content-md-end" id="navbarSupportedContent"> 
+					
+					{localStorage.getItem('token') ? <> 
+					<Link  to={ localStorage.getItem("type")== "user" ?"/area-privada-usuario" : "/area-privada-proveedor"}> 
+					   
+					</Link>
+					  <Link className="btn btn-outline-success rounded-pill" to="/area-privada-usuario">Área privada</Link>   {/* Terminar de implantar para user/supplier, y que solo aparezca en la Home */}
+					  <Link className="btn btn-outline-success rounded-pill" to="/area-de-favoritos">Mis favoritos</Link>
+					  <Link className="btn btn-outline-success rounded-pill" to="/" onClick={SessionOut}>Cerrar sesión</Link>
+					</> 
+					 :<>
+					<Link className="btn btn-outline-success rounded-pill " to="/registro"> 
+					   Registrarse
+					</Link>
+					   <Link className="btn btn-outline-success rounded-pill " to="/inicio-sesion">
+					   Iniciar sesión
+					   </Link> </>
+						  }
+					  </div>
+					</div>
+					</nav>
+							
+		);
+	};
+	
