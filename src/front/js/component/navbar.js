2457c452ce9fd7615dfb7e5fb8e496bc1cf90088
KGTH <kgthcruz94@gmail.com>
2023-02-04 19:43:13 +0000

    se modifica offer y navbar// espero que aparezca mañana

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -34,7 +34,7 @@ export const Navbar = () => {
 					   
 					</Link>
 					  <Link className="btn btn-outline-success rounded-pill" to="/area-privada-usuario">Área privada</Link>   {/* Terminar de implantar para user/supplier, y que solo aparezca en la Home */}
-					  <Link className="btn btn-outline-success rounded-pill" to="/area-de-favoritos">Mis favoritos</Link>
+					  {/* <Link className="btn btn-outline-success rounded-pill" to="/area-de-favoritos">Mis favoritos</Link>    // creo que ya no es necesario */}
 					  <Link className="btn btn-outline-success rounded-pill" to="/" onClick={SessionOut}>Cerrar sesión</Link>
 					</> 
 					 :<>
@@ -43,8 +43,10 @@ export const Navbar = () => {
 					</Link>
 					   <Link className="btn btn-outline-success rounded-pill " to="/inicio-sesion">
 					   Iniciar sesión
-					   </Link> </>
+					   </Link> 
+					   </>
 						  }
+						  
 					  </div>
 					</div>
 					</nav>
