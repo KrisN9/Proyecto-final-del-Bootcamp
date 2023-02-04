2c814fb4fdd0fb4c3333daf8e1e2b600e8ed3c3f
KGTH <kgthcruz94@gmail.com>
2023-02-04 20:47:55 +0000

    espero que se vean mis cambio =(

--- src/front/js/component/slide.js
+++ src/front/js/component/slide.js
@@ -5,10 +5,10 @@ const Slide=()=>{
 
     return (
           
-        <div id="carouselExampleFade" className="carousel slide carousel-fade container col-md-10" data-bs-ride="carousel">
+        <div id="carouselExampleFade" className="carousel slide carousel-fade " data-bs-ride="carousel">  {/*container col-md-10  // pendiente ver si queda mejor asi */ }
         <div className="carousel-inner  ">
           <div className="carousel-item active ">
-            <img src="https://www.hotelmalagapremium.com/wp-content/uploads/2018/03/MPH-noticia-sushi-take-away.jpg" className="d-block w-100 " height="600" alt="..."/>
+            <img src="https://www.hotelmalagapremium.com/wp-content/uploads/2018/03/MPH-noticia-sushi-take-away.jpg" className="d-block w-100" height="600" alt="..."/>
           </div>
           <div className="carousel-item">
             <img src="https://www.theoutletstoresalicante.es/wp-content/uploads/2019/03/TacoBell_Happy-Day-Adaptaciones_02_FB-1200x1200.png" className="d-block w-100"  height="600" alt="..."/>
@@ -20,7 +20,7 @@ const Slide=()=>{
             <img src="https://www.subwaypanama.com/content/20230103085737-1.jpg " className="d-block w-100 " height="600" alt="..."/>
           </div>
           <div className="carousel-item">
-            <img src="https://static.kfc.es/images/static-page/promo/megabox3/hero-desktop.jpg" className="d-block w-100 " height="500" alt="..."/>
+            <img src="https://static.kfc.es/images/static-page/promo/megabox3/hero-desktop.jpg" className="d-block w-100 " height="600" alt="..."/>
           </div>
         </div>
         <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
