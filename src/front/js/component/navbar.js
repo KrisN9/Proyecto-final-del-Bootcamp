16fb75b53d01170aceed19b5f437c4b13734c287
KrisN9 <kristinank992@gmail.com>
2023-02-05 23:55:16 +0000

    añadido detalle que faltaba para probar modificacion en navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { Context } from "../store/appContext"; 
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 
 export const Navbar = () => {
+	/* const location = useLocation(); */
 	const navigate = useNavigate();
     const {store , actions}= useContext(Context)
 
