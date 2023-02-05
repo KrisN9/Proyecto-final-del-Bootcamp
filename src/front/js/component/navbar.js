0dab84fad1ccfeec0797cf30331915cbf3b33383
KrisN9 <kristinank992@gmail.com>
2023-02-05 23:39:42 +0000

    cambio en navbar guardado en su rama

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,37 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../store/appContext"; 
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
+	const navigate = useNavigate();
+    const {store , actions}= useContext(Context)
 
-		<nav className="navbar navbar-expand-lg navbar-dark text-dark">
-			  <div className="container">
+          const SessionOut =()=>{
+            actions.logOut()
+            navigate("/");
+          }
+  
 
-   				<Link to="/">
-					<span className="navbar-brand mb-0 h1">
-					<img src="https://www.barrabes.com/images/home/logo_flash.png?v=2" alt="Logo" width="100" height="50" className="d-inline-block align-text-top"/>
-					</span>
-				</Link>
-				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-				<span className="navbar-toggler-icon"></span>  
-				</button>
-				<div className="collapse navbar-collapse gap-2 justify-content-md-end" id="navbarSupportedContent">
-				<div className="dropdown">
-				<button className="btn btn-outline-success rounded-pill dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-						Favorito
-				</button>
-				<ul className="dropdown-menu">
-				<p className="text-center fst-italic text-danger">Aún no tienes ofertas favoritas. <i class="far fa-sad-cry"></i></p>
-				</ul>
-				</div>
+		  return (
+					
+			<nav className="navbar navbar-expand-lg navbar-dark text-dark">
+				  <div className="container ">
+	
+					   <Link to="/">
+						<span className="navbar-brand mb-0 h1">
+						<img src="https://i.postimg.cc/KYLQqTks/Promo-Food-1.png" alt="Logo" width="70" height="70" className="d-inline-block align-text-top rounded-circle"/>
+						</span>
+					</Link>
+					
+					<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>  
+					</button>
+					<div className="collapse navbar-collapse gap-2 justify-content-md-end" id="navbarSupportedContent"> 
+					
+					{localStorage.getItem('token') ? <> 
+					<Link  to={ localStorage.getItem("type")== "user" ?"/area-privada-usuario" : "/area-privada-proveedor"}> 
+					   
+					</Link>
+					{location.pathname !== "/area-privada-usuario" &&
+					  <Link className="btn btn-outline-success rounded-pill" to="/area-privada-usuario">Área privada</Link>}
+					  <Link className="btn btn-outline-success rounded-pill" to="/" onClick={SessionOut}>Cerrar sesión</Link>
+					</> 
+					 :<>
+					<Link className="btn btn-outline-success rounded-pill " to="/registro"> 
+					   Registrarse
+					</Link>
+					   <Link className="btn btn-outline-success rounded-pill " to="/inicio-sesion">
+					   Iniciar sesión
+					   </Link> 
+					   </>
+						  }
+						  
+					  </div>
+					</div>
+					</nav>
 							
-			<Link to="/registro"><button type="button" className="btn btn-outline-success rounded-pill" >Registrarse</button></Link>
-			
-	 		<Link to="/inicio-sesion"><button type="button" className="btn btn-outline-success rounded-pill">Iniciar sesión</button></Link>
-			
-		</div>
-  </div>
-</nav>
-		
-	);
-};
+		);
+	};
+	
