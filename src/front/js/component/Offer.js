4e814fb01c00737a3152fa1d71bebaa348c68c37
KGTH <kgthcruz94@gmail.com>
2023-02-05 21:27:07 +0000

    input limpios de offer

--- src/front/js/component/Offer.js
+++ src/front/js/component/Offer.js
@@ -1,43 +1,43 @@
 import React from "react";
 import { useState } from "react";
 
-const Offer = () => { 
+const Offer = () => {
   const [formData, setFormData] = useState([]);
 
-  
-  const handleChange = (event) => { 
-    setFormData({...formData, [event.target.name]: event.target.value });
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
   };
-  
-  
-  const handleClick = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    fetch(process.env.BACKEND_URL + "/api/offer",{
-      method:"POST",
-      headers: {
-        "Content-Type": "application/json",
-        'Authorization': 'Bearer '+ localStorage.getItem('token')
-      },
-      body: JSON.stringify(formData),
-    }).then((response) => {
-      if (response.status == 200) { 
-        alert ("Se ha creado con exito")
-        setUser("")
-      }else {
-        alert ("algo ha salido mal! Intentalo de nuevo ")
-        setUser("")
-      }
-      response.json();
-    })
-    .then((response) => {
-    console.log(response);
-    });
+    // event.target.reset();
     
   };
-  
+
+  const handleClick = () => {
+    setFormData("");
+    fetch(process.env.BACKEND_URL + "/api/offer", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("token"),
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => {
+        if (response.status == 200) {
+          alert("Se ha creado con exito");
+        } else {
+          alert("algo ha salido mal! Intentalo de nuevo ");
+        }
+        response.json();
+      })
+      .then((response) => {
+        console.log(response);
+      });
+  };
 
   return (
-    <form onSubmit={handleClick}>
+    <form onSubmit={handleSubmit}>
       <div className="text-center">
         <p className="fs-1 fw-bolder">Añade tu oferta</p>
       </div>
@@ -50,18 +50,20 @@ const Offer = () => {
             placeholder="Nombre de la empresa "
             name="company_name"
             onChange={handleChange}
+            value={formData.company_name}
           />
           <label htmlFor="floatingName">Nombre de la empresa </label>
         </div>
-      
+
         <div className="form-floating mb-3">
           <input
             type="text"
-            className="form-control"   
+            className="form-control"
             id="floatingTitle"
             placeholder="Descripción de la oferta "
             name="title"
             onChange={handleChange}
+            value={formData.title}
           />
           <label htmlFor="floatingTitle">Descripción de la oferta</label>
         </div>
@@ -73,11 +75,11 @@ const Offer = () => {
             placeholder="Precio de oferta"
             name="price"
             onChange={handleChange}
-           
+            value={formData.price}
           />
           <label htmlFor="floatingPrice">Precio de oferta</label>
         </div>
-       <div className="form-floating mb-3">
+        <div className="form-floating mb-3">
           <input
             type="url"
             className="form-control"
@@ -85,7 +87,7 @@ const Offer = () => {
             placeholder="url"
             name="url"
             onChange={handleChange}
-           
+            value={formData.url}
           />
           <label htmlFor="floatingPrice">Dirección página web</label>
         </div>
@@ -97,29 +99,31 @@ const Offer = () => {
             placeholder="location"
             name="location"
             onChange={handleChange}
-            
+            value={formData.location}
           />
           <label htmlFor="floatingLocation">location</label>
         </div>
         <div className="mb-3 text-start ">
-          <label htmlFor="floatingImage" className="form-label">Añadir Imagen</label>
-          <input className="form-control form-control-sm" 
-          id="floatingImage" 
-          name="url_image" 
-          type="file"
-          onChange={handleChange}
-          
+          <label htmlFor="floatingImage" className="form-label">
+            Añadir Imagen
+          </label>
+          <input
+            className="form-control form-control-sm"
+            id="floatingImage"
+            name="url_image"
+            type="file"
+            onChange={handleChange}
           />
-       </div>
-      
+        </div>
+
         <div className="col-12 mb-3">
           <button
-            type="submit"
+            type="reset"
             className="btn btn-danger"
             onClick={handleClick}
           >
             Enviar
-          </button>    
+          </button>
           {/* <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div className="modal-dialog">
     <div className="modal-content">
@@ -137,9 +141,6 @@ const Offer = () => {
     </div>
   </div>
 </div> */}
-
-          
-
         </div>
       </div>
     </form>
@@ -148,20 +149,21 @@ const Offer = () => {
 
 export default Offer;
 
-
-
-{/* { upDate  ? "Ha fallado algo! Intentalo de nuevo" : "Tu oferta ha sido creada con exito " }  */}
+{
+  /* { upDate  ? "Ha fallado algo! Intentalo de nuevo" : "Tu oferta ha sido creada con exito " }  */
+}
 
 // <div className="mb-3 text-start">
 // <label htmlFor="floatingUrl" className="form-label">Añadir url de empresa </label>
 // <input className="form-control form-control-sm"
-//  id="floatingUrl" 
-//  name="url" 
+//  id="floatingUrl"
+//  name="url"
 //  type="file"
 //  onChange={handleChange}/>
 // </div>
 
-{/* <div className="form-floating mb-3">
+{
+  /* <div className="form-floating mb-3">
           <input
             type="name"
             className="form-control"
@@ -171,4 +173,5 @@ export default Offer;
             onChange={handleChange}
           />
           <label htmlFor="floatingName">Nombre del proveedor </label>
-        </div> */}
+        </div> */
+}
