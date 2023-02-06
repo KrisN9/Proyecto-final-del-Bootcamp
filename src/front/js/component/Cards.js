7a4c7c1f18f1aad852878500c655eba8bdd6b50a
KGTH <kgthcruz94@gmail.com>
2023-02-06 13:03:04 +0000

    se actualiza cards a su rama

--- src/front/js/component/Cards.js
+++ src/front/js/component/Cards.js
@@ -1,39 +1,101 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const Cards =()=>{
+const Cards = () => {
+  const [favorites, setFavorites] = useState([]);
 
-    return (
+  useEffect(() => {
+    getFavorite();
+  }, []);
+  
+  const getFavorite = () => {
+    fetch(process.env.BACKEND_URL + "/api/user/favorite", {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("token"),
+      },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        setFavorites(response);
+      });
+  }
 
-         /* pendiente de revisar si van las cards 
-         usar props, useffect */ 
-         
-        <div className="card mb-3">
-        <div className="row g-0">
-            <div className="col-md-3">
-            <img src="https://www.dominospizza.es/images/2x1alabama_delivery_RESP.jpg" className="img-fluid rounded-start" alt="..."/>
-            </div>
-            <div className="col-md-7">
-            <div className="card-body text-center">
-                <h5 className="card-title fs-3 fw-bolder">Dominos Pizza</h5>
-                <p className="card-text"> 2x1 En todas las masas y tamaños</p>
-            </div>
-            </div>
-            <div className="col-md-2 mt-3">
-                <button type="button" className="btn btn-danger float-end me-3 mt-2">
-                <i className="fas fa-heart"></i>
-                </button>
-                <button type="button" className="btn btn-warning float-end me-2 mt-2">Pedir!</button>
-                </div>
-        </div>
-        </div>
-     
-
-
+  const removeFavorite = (favorite_id) => {
+    console.log(favorite_id)
+    const constfavorite = {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("token"),
+      },
+    };
+    fetch(
+      process.env.BACKEND_URL + "/api/delete_favorite/" + favorite_id,
+      constfavorite
     )
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        getFavorite();
+      });
+  };
 
+  return (
+    <div className="container col-12 col-md-6 mt-4">
+      <div className="mt-3">
+        {favorites.map((favorite) => {
+          return (
+            <div className="card mb-3" key={favorite.id}>
+              <div className="row g-0">
+                <div className="col-md-3">
+                  <img
+                    src={favorite.offerlist.url_image}
+                    className="img-fluid rounded-start"
+                    alt="..."
+                  />
+                </div>
+                <div className="col-md-7">
+                  <div className="card-body text-center">
+                    <h5 className="card-title fs-3 fw-bolder">
+                      {favorite.offerlist.company_name}
+                    </h5>
+                    <p className="card-text"> {favorite.offerlist.title}</p>
+                  </div>
+                </div>
+                <div className="col-md-2 mt-2">
+                  <p>Precio:{favorite.offerlist.price}€</p>
+                  <a
+                    href={favorite.offerlist.url}
+                    target="_blanck"
+                    type="button"
+                    className="btn btn-warning float-end me-2 "
+                  >
+                    Pedir!
+                  </a>
+                  <button
+                    type="button"
+                    className="btn btn-outline-secondary mt-2 mb-2"
+                    onClick={() => removeFavorite(favorite.id)}
+                  >
+                    <i className="fas fa-trash-alt"></i>
+                  </button>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
+
+{
+  /* <button type="button" className="btn btn-danger float-end me-3">
+                    <i className="fas fa-heart"></i>
+                  </button> */
 }
-
-
-
-
-export default Cards; 
