69c743e9f4f3922f6583af731e12e4108c83dcfc
KGTH <kgthcruz94@gmail.com>
2023-02-06 15:03:39 +0000

    se actualiza area privada de supplier

---
+++ src/front/js/component/PrivateAreaSupplier.js
@@ -0,0 +1,74 @@
+import React, { useState ,useEffect } from "react";
+import Offer from "./Offer";
+import OfferCards from "./OfferCards";
+import Supplier from "./Supplier";
+
+
+
+
+const PrivateAreaSupplier=()=>{
+  const [supplier , setSupplier] = useState([]);
+ 
+
+  useEffect(()=>{
+    const optionsSupplier = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: 'Bearer '+ localStorage.getItem("token"),
+      },
+    };
+    fetch(process.env.BACKEND_URL + "/api/supplier", optionsSupplier)
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        setSupplier(response);
+      });
+  }, [])
+
+return (
+    <div className="container mt-3">
+        <div className="text-center">
+          <div className="mt-5 mb-5">
+        <p className="fst-italic fs-2">Bienvenid@ a tu area privada {supplier.name}</p>
+        </div>
+  <ul className="nav nav-tabs  justify-content-center"  id="myTab" role="tablist">
+  <li className="nav-item" role="presentation">
+    <button className="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home-tab-pane" type="button" role="tab" aria-controls="home-tab-pane" aria-selected="true">Cuenta</button>
+  </li>
+  <li className="nav-item" role="presentation">
+    <button className="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile-tab-pane" type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false">Tus Ofertas</button>
+  </li>
+  <li className="nav-item" role="presentation">
+    <button className="nav-link" id="offer" data-bs-toggle="tab" data-bs-target="#offer-pane" type="button" role="tab" aria-controls="offer-pane" aria-selected="false">Añadir Ofertas</button>
+  </li>
+  
+  
+ 
+</ul>
+<div className="tab-content" id="myTabContent">
+  <div className="tab-pane fade show active" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab" tabindex="0">
+  <div className="container col-md-8 text-md-start mt-5 mb-5 fs-4 fw-semibold">
+       <Supplier/>
+  </div>
+  </div>
+  <div className="tab-pane fade" id="profile-tab-pane" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">
+    
+    <OfferCards/>  
+  </div>
+  
+
+  <div className="tab-pane fade" id="offer-pane" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">
+    <div>
+       <Offer/>
+    </div>
+  </div>
+</div>
+        </div>
+    </div>
+
+)
+   
+}
+
+export default  PrivateAreaSupplier
