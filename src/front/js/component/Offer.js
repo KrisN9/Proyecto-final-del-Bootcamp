dd1374d1abaaeaaac28f94f445f7cf10d958ad7c
KGTH <kgthcruz94@gmail.com>
2023-02-06 13:42:31 +0000

    se actualiza offer

---
+++ src/front/js/component/Offer.js
@@ -0,0 +1,179 @@
+import React from "react";
+import { useState } from "react";
+
+const Offer = () => {
+  const [formData, setFormData] = useState([]);
+
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // event.target.reset();
+    
+  };
+
+  const handleClick = () => {
+    setFormData("");
+    fetch(process.env.BACKEND_URL + "/api/offer", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("token"),
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => {
+        if (response.status == 200) {
+          alert("Se ha creado con exito");
+        } else {
+          alert("algo ha salido mal! Intentalo de nuevo ");
+        }
+        response.json();
+      })
+      .then((response) => {
+        console.log(response);
+      });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="text-center">
+        <p className="fs-1 fw-bolder">Añade tu oferta</p>
+      </div>
+      <div className="container bg-warning px-3 py-3 mt-2 mb-5 col-12 col-md-8">
+        <div className="form-floating mb-3">
+          <input
+            type="text"
+            className="form-control"
+            id="floatingName"
+            placeholder="Nombre de la empresa "
+            name="company_name"
+            onChange={handleChange}
+            value={formData.company_name}
+          />
+          <label htmlFor="floatingName">Nombre de la empresa </label>
+        </div>
+
+        <div className="form-floating mb-3">
+          <input
+            type="text"
+            className="form-control"
+            id="floatingTitle"
+            placeholder="Descripción de la oferta "
+            name="title"
+            onChange={handleChange}
+            value={formData.title}
+          />
+          <label htmlFor="floatingTitle">Descripción de la oferta</label>
+        </div>
+        <div className="form-floating mb-3">
+          <input
+            type="price"
+            className="form-control"
+            id="floatingPrice"
+            placeholder="Precio de oferta"
+            name="price"
+            onChange={handleChange}
+            value={formData.price}
+          />
+          <label htmlFor="floatingPrice">Precio de oferta</label>
+        </div>
+        <div className="form-floating mb-3">
+          <input
+            type="url"
+            className="form-control"
+            id="floatingUrl"
+            placeholder="url"
+            name="url"
+            onChange={handleChange}
+            value={formData.url}
+          />
+          <label htmlFor="floatingPrice">Dirección página web</label>
+        </div>
+        <div className="form-floating mb-3">
+          <input
+            type="location"
+            className="form-control"
+            id="floatingLocation"
+            placeholder="location"
+            name="location"
+            onChange={handleChange}
+            value={formData.location}
+          />
+          <label htmlFor="floatingLocation">location</label>
+        </div>
+
+
+        <div className="form-floating mb-3">
+          <input
+            type="url_image"
+            className="form-control"
+            id="floatingUrlImage"
+            placeholder="imagen"
+            name="url_image"
+            onChange={handleChange}
+          />
+          <label htmlFor="floatingName">Url de imagen  </label>
+        </div> 
+       
+
+        <div className="col-12 mb-3">
+          <button
+            type="reset"
+            className="btn btn-danger"
+            onClick={handleClick}
+          >
+            Enviar
+          </button>
+          {/* <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div className="modal-dialog">
+    <div className="modal-content">
+      <div className="modal-header">
+        <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div className="modal-body">
+       hola
+      </div>
+      <div className="modal-footer">
+        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" className="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div> */}
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default Offer;
+
+{
+  /* { upDate  ? "Ha fallado algo! Intentalo de nuevo" : "Tu oferta ha sido creada con exito " }  */
+}
+
+// <div className="mb-3 text-start">
+// <label htmlFor="floatingUrl" className="form-label">Añadir url de empresa </label>
+// <input className="form-control form-control-sm"
+//  id="floatingUrl"
+//  name="url"
+//  type="file"
+//  onChange={handleChange}/>
+// </div>
+
+
+// <div className="mb-3 text-start ">
+// <label htmlFor="floatingImage" className="form-label">
+//   Añadir Imagen
+// </label>
+// <input
+//   className="form-control form-control-sm"
+//   id="floatingImage"
+//   name="url_image"
+//   type="file"
+//   onChange={handleChange}
+// />
+// </div>
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,25 +2,53 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import "../../styles/sidebar.css";
+import Slide from "../component/slide";
+import ContactForm from "../component/ContactForm";
+import SupplierRegister from "../component/SupplierRegister";
+import UserRegister from "../component/UserRegister";
+import Cards from "../component/Cards";
+import FinalMap from "../component/FinalMap";
+import Sidebar from "../component/componentes de prueba/Sidebar";
+import { Link } from "react-router-dom";
+import OfferList from "../component/OfferList";
+
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
-		</div>
-	);
+  return (
+    <>
+      <div className="text-center mt-3 back">
+        <div className="container squar1 mt-5 mb-3">
+          <p className="fs-1 text-success fst-italic">
+            Aquí encontrarás tus restaurantes favoritos con las mejores ofertas.
+          </p>
+        </div>
+        <br></br>
+        <Slide />
+
+        <br></br>
+
+        <div className="row">
+          <div className="container squar1 mt-5 mb-3">
+            <h3 className="fs-1 text-success fst-italic">
+              ¡Si te gustan estas ofertas, busca en el mapa las más cercanas a
+              ti!
+            </h3>
+          </div>
+          <div className="container col-md-10  mt-4 mb-5 ">
+            {/* <Sidebar/> */}
+            <div className="map ">
+              <FinalMap />
+            </div>
+          </div>
+        </div>
+			<br></br>
+
+			
+			
+      </div>
+    </>
+  );
 };
