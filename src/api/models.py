a950bf8aa6e67d86917a0e87f33ccce2b98bd025
KGTH <kgthcruz94@gmail.com>
2023-03-06 17:56:15 +0000

    pendiente city

--- src/api/models.py
+++ src/api/models.py
@@ -22,7 +22,7 @@ class User(db.Model):
             "id": self.id,
             "email": self.email,
             "name":self.name,
-            "telephone_number":self.telephone_number,
+            "telephone_number":self.telephone_number,"city":self.city
             
 
 
